3c064b5d8d2cc89c2bdbf46571498bf9eb09bb3a
Kylen Solvik <kysolvik@gmail.com>
2024-01-16 10:28:46 -0700

    Put number-swapped files into new directory

--- reservoir-id-smp/predict/quick_swap_num_order.sh
+++ reservoir-id-smp/predict/quick_swap_num_order.sh
@@ -1,4 +1,5 @@
 # Fix for bad projection originally
+mkdir -p out2
 for f in ./out/*
-    do mv $f $(echo $f | sed -r 's/^.*pred_([0-9]*)-([0-9]*).*/out\/pred_\2-\1.tif/')
+    do mv $f $(echo $f | sed -r 's/^.*pred_([0-9]*)-([0-9]*).*/out2\/pred_\2-\1.tif/')
 done

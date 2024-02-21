ed90ab5d370d5b52c643fe845893e4b058958a15
Kylen Solvik <kysolvik@gmail.com>
2024-02-21 17:31:16 -0500

    Add option to specify number of center pixels to extract for match_tiles.py

--- reservoir-id-smp/annotation_prep/match_tiles.py
+++ reservoir-id-smp/annotation_prep/match_tiles.py
@@ -37,19 +37,38 @@ def argparse_init():
     p.add_argument('output_suffix',
                    help='Output suffix, e.g. sent 2_20m',
                    type=str)
+    p.add_argument('--center_pixels_num',
+                   help='Create tiles out of N pixels (e.g. 500 for 500x500)' ,
+                   type=int,
+                   default=None)
 
     return p
 
-def subset_target(target_raster, output_file, subset_df_row):
-    xmin = str(subset_df_row['lon_min'])
-    xmax = str(subset_df_row['lon_max'])
-    ymin = str(subset_df_row['lat_min'])
-    ymax = str(subset_df_row['lat_max'])
+def subset_target(target_raster, output_file, subset_df_row, center_pixels):
+    if center_pixels is not None:
+        xmin = subset_df_row['lon_min']
+        xmax = subset_df_row['lon_max']
+        ymin = subset_df_row['lat_min']
+        ymax = subset_df_row['lat_max']
+
+        total_pixels_x = round((xmax-xmin)/TARGET_RES)
+        total_pixels_y = round((ymax-ymin)/TARGET_RES)
+        padding_x = (total_pixels_x - center_pixels)/2
+        padding_y = (total_pixels_y - center_pixels)/2
+        xmin = str(xmin + padding_x*TARGET_RES)
+        xmax = str(xmax - padding_x*TARGET_RES)
+        ymin = str(ymin + padding_y*TARGET_RES)
+        ymax = str(ymax - padding_y*TARGET_RES)
+    else:
+        xmin = str(subset_df_row['lon_min'])
+        xmax = str(subset_df_row['lon_max'])
+        ymin = str(subset_df_row['lat_min'])
+        ymax = str(subset_df_row['lat_max'])
     sp.call(['gdalwarp', '-tr', str(TARGET_RES), str(TARGET_RES),
              '-te', xmin, ymin, xmax, ymax, '-overwrite', '-co', 'COMPRESS=LZW',
              target_raster, output_file])
 
-    return 
+    return
 
 
 def main():
@@ -69,7 +88,8 @@ def main():
         cur_row = grid_df.loc[row_i]
         output_file = '{}/{}.tif'.format(args.output_dir, cur_row['name'].replace(
             'ndwi', args.output_suffix))
-        subset_target(args.target_raster, output_file, cur_row)
+        subset_target(args.target_raster, output_file, cur_row,
+                center_pixels=args.center_pixels_num)
 
     return
 

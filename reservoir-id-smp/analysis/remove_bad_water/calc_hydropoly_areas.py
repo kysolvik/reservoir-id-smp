41fdd28fed378bedb252375ce345206a9a33a0ac
Kylen Solvik <kysolvik@gmail.com>
2024-03-30 13:08:26 -0600

    Calculate hydropoly areas instead of filtering out the small ones

---
+++ reservoir-id-smp/analysis/remove_bad_water/calc_hydropoly_areas.py
@@ -0,0 +1,18 @@
+"""
+Calculate hydropoly areas in ha and round to int before rasterizing
+"""
+
+import numpy as np
+import geopandas as gpd
+
+
+brazil_shp = '/home/ksolvik/research/reservoirs/analysis/data/misc/general_borders/Brazil_10kmbuffer_noremoteislands_noholes.shp'
+hydropolies_gdb = './data/hydropolys.gdb'
+
+
+brazil = gpd.read_file('../../../../../analysis/data/misc/general_borders/Brazil_10kmbuffer_noremoteislands_noholes.shp')
+hp_gdf = gpd.read_file(hydropolies_gdb, mask=brazil)
+hp_gdf['area_ha'] = hp_gdf.to_crs('ESRI:102033').area * 0.0001
+hp_gdf.loc[hp_gdf['area_ha'] > 254, 'area_ha'] = 254
+hp_gdf['area_ha'] = hp_gdf['area_ha'].astype(np.uint8)
+hp_gdf.to_file('./data/hydropolys_area_ha.shp')

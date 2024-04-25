c0f32270f2b9c54257a2362ddbfcc6b5c52dcea5
Kylen Solvik <kysolvik@gmail.com>
2024-04-25 15:13:45 -0600

    Update to rio, calc com, and use pandas for writing

--- reservoir-id-smp/analysis/area_count/calc_areas_regions.py
+++ reservoir-id-smp/analysis/area_count/calc_areas_regions.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 """ Quick script to get counts and distribution of reservoir areas'"""
 
-from osgeo import gdal
 import numpy as np
 import sys
 from scipy import ndimage, stats
 import os
 from PIL import Image
+import rasterio as rio
+from rasterio.windows import Window
+import pandas as pd
 
 
 tif = sys.argv[1]
 region_tif = sys.argv[2]
 properties_tif = sys.argv[3]
-out_txt = sys.argv[4]
+out_csv = sys.argv[4]
 labeled_tif = tif.replace('.tif', '_labeled.tif')
 COMPARE_TO_PREVIOUS = True
 y = os.path.basename(tif)[9:13]
@@ -20,9 +22,9 @@ last_labeled_tif = labeled_tif.replace(y, str(int(y)-1))
 
 box_size = 200000
 
-fh = gdal.Open(tif)
-region_fh = gdal.Open(region_tif)
-property_fh = gdal.Open(properties_tif)
+fh = rio.open(tif)
+region_fh = rio.open(region_tif)
+property_fh = rio.open(properties_tif)
 
 
 def convert_row_col_to_xy(affine_transformer, centers,
@@ -107,7 +109,6 @@ def compare_previous_year(label_im, label_values, last_label_im):
             [0])
     return previous_all
 
-
 total_rows, total_cols = fh.RasterYSize, fh.RasterXSize
 current_row = 0
 current_col = 0
@@ -117,17 +118,23 @@ col_starts = np.arange(0, total_cols, box_size)
 # Create Nx2 array with row/col start indices
 start_ind = np.array(np.meshgrid(row_starts, col_starts)).T.reshape(-1, 2)
 
-with open(out_txt, 'w') as f:
-    f.write('label,area,reg,property_mode,property_all,previous_labels\n')
+# Get some affine information
+cur_transform = fh.transform
+affine_transformer = rio.transform.AffineTransformer(cur_transform)
+
+# Set numpy print options so it doesn't abbreviate anything
+np.set_printoptions(threshold=4*box_size)
+
 for i in range(start_ind.shape[0]):
     # For the indices near edge we need to use a smaller box size
     box_size_rows = min(total_rows - start_ind[i, 0], box_size)
     box_size_cols = min(total_cols - start_ind[i, 1], box_size)
+    start_ind_row, start_ind_col = (start_ind[i,0], start_ind[i,1])
 
     # Get base counts and labeled image
-    ar = fh.GetRasterBand(1).ReadAsArray(
-        int(start_ind[i, 1]), int(start_ind[i, 0]),
-        int(box_size_cols), int(box_size_rows))
+    read_window = Window(int(start_ind_col), int(start_ind_row),
+                         int(box_size_cols), int(box_size_rows))
+    ar = fh.read(1, window=read_window)
     if (ar == 1).sum() > 0:
         sizes, label_im, label_values = get_labels_count(ar)
 
@@ -137,52 +144,68 @@ for i in range(start_ind.shape[0]):
             im.save(labeled_tif, compression='tiff_lzw')
             # If last labeled image exist, 
             if os.path.isfile(last_labeled_tif):
-                prev_fh = gdal.Open(last_labeled_tif)
-                prev_label_ar = prev_fh.GetRasterBand(1).ReadAsArray()
+                prev_fh = rio.open(last_labeled_tif)
+                prev_label_ar = prev_fh.read(1)
                 previous_labels = compare_previous_year(
                         label_im, label_values, prev_label_ar)
-                write_labels = True
             else:
-                write_labels = False
+                previous_labels = np.zeros_like(label_values)
 
         # Get region stats
-        reg_ar = region_fh.GetRasterBand(1).ReadAsArray(
-            int(start_ind[i, 1]), int(start_ind[i, 0]),
-            int(box_size_cols), int(box_size_rows))
+        reg_ar = region_fh.read(1, window=read_window)
         if (reg_ar != 255).sum() > 0:
             regions = get_region_stats(label_im, label_values, reg_ar)
 
             # Get property stats
-            prop_ar = property_fh.GetRasterBand(1).ReadAsArray(
-                int(start_ind[i, 1]), int(start_ind[i, 0]),
-                int(box_size_cols), int(box_size_rows))
+            prop_ar = property_fh.read(1, window=read_window)
             property_mode, property_all = get_property_stats(
                     label_im, label_values, prop_ar)
 
             # Centers of mass
             centers_of_mass = get_centers(label_im, label_values)
-            centers_lon, centers_lat = convert_row_col_to_xy(
+            centers_x, centers_y = convert_row_col_to_xy(
                     affine_transformer, centers_of_mass,
                     start_ind_row, start_ind_col)
 
             print('Stats done')
 
-            with open(out_txt, 'a') as f:
-                for i in range(len(sizes)):
-                    if COMPARE_TO_PREVIOUS and write_labels:
-                        f.write("{},{},{},{},{},{}\n".format(
-                            label_values[i],
-                            int(sizes[i]),
-                            regions[i],
-                            property_mode[i],
-                            property_all[i],
-                            previous_labels[i])
-                            )
-                    else:
-                        f.write("{},{},{},{},{}\n".format(
-                            label_values[i],
-                            int(sizes[i]),
-                            regions[i],
-                            property_mode[i],
-                            property_all[i])
-                            )
+        if COMPARE_TO_PREVIOUS and write_labels:
+            out_dict = {
+                    'id': label_values,
+                    'label': label_values,
+                    'row_start': start_ind_row,
+                    'col_start': start_ind_col,
+                    'box_rows': box_size_rows,
+                    'box_cols': box_size_cols,
+                    'area': sizes,
+                    'reg': regions,
+                    'property_mode': property_mode,
+                    'property_all': property_all,
+                    'previous_labels': previous_labels
+                    'center_col': centers_of_mass[:, 1],
+                    'center_row': centers_of_mass[:, 0],
+                    'center_y': centers_y,
+                    'center_x': centers_y,
+                    }
+        else:
+            out_dict = {
+                    'id': label_values,
+                    'label': label_values,
+                    'row_start': start_ind_row,
+                    'col_start': start_ind_col,
+                    'box_rows': box_size_rows,
+                    'box_cols': box_size_cols,
+                    'area': sizes,
+                    'reg': regions,
+                    'property_mode': property_mode,
+                    'property_all': property_all,
+                    'center_col': centers_of_mass[:, 1],
+                    'center_row': centers_of_mass[:, 0],
+                    'center_y': centers_y,
+                    'center_x': centers_y,
+                    }
+            out_df = pd.DataFrame(out_dict)
+
+            out_df.to_csv(out_csv, mode='a',
+                          header=(not os.path.isfile(out_csv)),
+                          index=False)

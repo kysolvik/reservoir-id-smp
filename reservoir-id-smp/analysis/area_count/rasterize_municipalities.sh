11eca3b0cc8f1a6f1e91b2b2d2461b17d668b982
Kylen Solvik <kysolvik@gmail.com>
2024-05-30 12:42:32 -0600

    Update rasterizee munis

--- reservoir-id-smp/analysis/area_count/rasterize_municipalities.sh
+++ reservoir-id-smp/analysis/area_count/rasterize_municipalities.sh
@@ -1,2 +1,2 @@
-gdal_rasterize -ot Byte -tr 10 10 -te 656477.922 1847011.958 915697.922 2396361.958 -tap -a "ID" -co "COMPRESS=LZW" -a_nodata 255 ../clip/brazil_munis_interviews_aea.shp ./data/munis_raster_aea.tif
+gdal_rasterize -ot Byte -tr 10 10 -te 656477.922 1847011.958 915697.922 2396361.958 -a "ID" -co "COMPRESS=LZW" -a_nodata 255 ../clip/brazil_munis_interviews_aea.shp ./data/munis_raster_aea_v2.tif
 

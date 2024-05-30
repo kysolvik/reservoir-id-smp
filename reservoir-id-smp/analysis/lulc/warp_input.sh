ce8b372aeaa54fe0995d9ff49be8451ad6c838c9
Kylen Solvik <kysolvik@gmail.com>
2024-05-30 12:43:46 -0600

    add speedups to lulc warp

--- reservoir-id-smp/analysis/lulc/warp_input.sh
+++ reservoir-id-smp/analysis/lulc/warp_input.sh
@@ -1,4 +1,4 @@
 for f in ./in/wgs84/*.tif;
 do
-    gdalwarp $f ${f/.tif/_aea_10m.tif} -tr 10 10 -t_srs ESRI:102033 -te 656477.922 1847011.958 915697.922 2396361.958 -co "COMPRESS=LZW" -srcnodata 0 -dstnodata 0 -wm 2000
+    gdalwarp $f ${f/.tif/_aea_10m.tif} -tr 10 10 -t_srs ESRI:102033 -te 656477.922 1847011.958 915697.922 2396361.958 -co "COMPRESS=LZW" -srcnodata 0 -dstnodata 0 -wm 4096 --config GDAL_CACHEMAX 4096
 done

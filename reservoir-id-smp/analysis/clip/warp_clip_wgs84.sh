397fd6b8f238365f44e3915284e40122c4848ffe
Kylen Solvik <kysolvik@gmail.com>
2024-07-20 08:45:47 -0600

    Warp/clip full outputs

--- reservoir-id-smp/analysis/clip/warp_clip_wgs84.sh
+++ reservoir-id-smp/analysis/clip/warp_clip_wgs84.sh
@@ -1,9 +1,11 @@
 input_dir='/home/ksolvik/research/reservoirs/code/reservoir-id-smp/reservoir-id-smp/predict/full_outputs/v2/'
+# input_dir='/home/ksolvik/research/reservoirs/code/reservoir-id-smp/reservoir-id-smp/predict/full_outputs/v2/interview_area/'
 
-for f in $input_dir/*.tif;
+# for f in $input_dir/*.tif;
+for f in $input_dir/*2019*.tif;
 do 
     f_base=${f##*/}
-    output_file="out/${f_base/.tif/_clip_wgs84.tif}"
+    output_file="out_interview_area/${f_base/.tif/_clip_wgs84.tif}"
     gdalwarp $f $output_file -co "COMPRESS=LZW" \
         -cutline './brazil_munis_interviews.geojson' -crop_to_cutline \
         -wm 2000 -multi -wo NUM_THREADS=4 -co "TILED=YES"

7f57ed89b5afffee1118a13f0e48de02df0596ba
Kylen Solvik <kysolvik@gmail.com>
2025-02-21 14:55:04 -0700

    Remove old area_count dir

--- reservoir-id-smp/analysis/area_count/quickrun_allyears.sh
+++
@@ -1,6 +0,0 @@
-for f in ../clip/out/*aea.tif;
-do 
-    f_base=${f##*/}
-    outfile="out/${f_base/.tif/_counts.csv}"
-    python3 calc_areas_regions.py $f ./data/munis_raster_aea_v2.tif ./data/properties_raster.tif $outfile
-done

8788e14f5cb2c11f60daeed6c30f62842279f34e
Kylen Solvik <kysolvik@gmail.com>
2025-10-02 17:40:12 -0400

    Rio merge script, now accepts args and creates mosaiced output tif

--- reservoir-id-smp/predict/rio_merge_overlaps.py
+++ reservoir-id-smp/predict/rio_merge_overlaps.py
@@ -5,13 +5,18 @@ from rasterio.merge import merge
 import os
 import subprocess as sp
 from rasterio.windows import Window
+import sys
 
 
+# Input dir with individual pred tiles
+IN_DIR = sys.argv[1] # Example: './out/out_allbrazil_v12_30m/sentinel/2021'
+# Output dir to store processed, saved tifs, each PROCESS_BOX_SIZE x PROCESS_BOX_SIZE
+OUT_DIR = sys.argv[2] # Example: './out/merged'
+
+# Side-length for processing tiles (10k to 20k seems pretty good)
 PROCESS_BOX_SIZE = 20000
-IN_DIR = './out_allbrazil_v12_30m/sentinel/2021'
-OUT_DIR = './merge_test3'
-
-os.makedirs(OUT_DIR)
+# If true, will create 'full_overlapped.tif' in OUT_DIR that is full mosaiced output
+CREATE_FULL_TIF = True
 
 
 def get_files(input_dir):
@@ -32,6 +37,8 @@ def filter_bounds(tile_bounds, rows, cols):
     """
     Args:
         tile_bounds (list): start_row, start_col, end_row, end_col
+        rows (list): List of integer row starts
+        cols (list): List of integer col starts
     """
 
     file_inds = ((rows > tile_bounds[0])
@@ -89,91 +96,79 @@ def write_out(avg_ar, out_path, out_fh, row_start, col_start):
     out_fh.write(avg_ar, window=out_window, indexes=1)
     
 
-# Get input files
-file_list = np.array(get_files(IN_DIR))
-tile_size = rio.open(file_list[0]).width
-rows_cols = np.array([row_col_start(fp) for fp in file_list])
+def main():
+    # Make output dir if it doesn't already exist
+    if not os.path.isdir(OUT_DIR):
+        os.makedirs(OUT_DIR)
 
-# Make a template vrt and set up output profile using it
-vrt_info = template_vrt_info(file_list)
-total_rows, total_cols = vrt_info.shape
+    # Get input files
+    file_list = np.array(get_files(IN_DIR))
+    tile_size = rio.open(file_list[0]).width
+    rows_cols = np.array([row_col_start(fp) for fp in file_list])
 
-# Build output raster profile
-out_profile = vrt_info.profile
-out_profile['driver'] = 'GTiff'
-out_profile['compress'] = 'LZW'
-out_profile['blockxsize'] = 256
-out_profile['blockysize'] = 256
+    # Make a template vrt and set up output profile using it
+    vrt_info = template_vrt_info(file_list, make_vrt=True)
+    total_rows, total_cols = vrt_info.shape
 
-# Process raster in blocks, each PROCESS_BOX SIZE x PROCESS_BOX_SIZE
-row_starts = np.arange(0, total_rows, PROCESS_BOX_SIZE)
-col_starts = np.arange(0, total_cols, PROCESS_BOX_SIZE)
+    # Build output raster profile
+    out_profile = vrt_info.profile
+    out_profile['driver'] = 'GTiff'
+    out_profile['compress'] = 'LZW'
+    out_profile['blockxsize'] = 256
+    out_profile['blockysize'] = 256
 
-start_ind = np.array(np.meshgrid(row_starts, col_starts)).T.reshape(-1, 2)
-for cur_ind in start_ind:
-    # Merge can't handle 300,000 files, so first we filter by indexes
-    tile_bounds = [cur_ind[0] - tile_size,
-                    cur_ind[1] - tile_size,
-                    cur_ind[0] + PROCESS_BOX_SIZE + tile_size,
-                    cur_ind[1] + PROCESS_BOX_SIZE + tile_size]
-    target_files = file_list[filter_bounds(tile_bounds, rows_cols[:, 0], rows_cols[:, 1])]
-    if len(target_files)> 0:
-        # Get geographic bounds info
-        geo_starts = vrt_info.transform * cur_ind
-        geo_ends = list(vrt_info.transform * (cur_ind + PROCESS_BOX_SIZE))
-        geo_ends[1] = max(geo_ends[1], vrt_info.bounds.bottom)
-        geo_ends[0] = min(geo_ends[0], vrt_info.bounds.right)
-        geo_bounds = (geo_starts[0], geo_ends[1], geo_ends[0], geo_starts[1])
+    # Process raster in blocks, each PROCESS_BOX SIZE x PROCESS_BOX_SIZE
+    row_starts = np.arange(0, total_rows, PROCESS_BOX_SIZE)
+    col_starts = np.arange(0, total_cols, PROCESS_BOX_SIZE)
 
-        avg, out_transform = merge_avg(target_files, geo_bounds)
-        if geo_starts[0] != out_transform.c or geo_starts[1] != out_transform.f:
-            print('geo bounds:', geo_bounds)
-            print('out_transform:', out_transform)
-            raise ValueError('Output transform does not match geo bounds')
+    start_ind = np.array(np.meshgrid(row_starts, col_starts)).T.reshape(-1, 2)
+    for cur_ind in start_ind:
+        # Merge can't handle 300,000 files, so first we filter by indexes
+        tile_bounds = [cur_ind[0] - tile_size,
+                        cur_ind[1] - tile_size,
+                        cur_ind[0] + PROCESS_BOX_SIZE + tile_size,
+                        cur_ind[1] + PROCESS_BOX_SIZE + tile_size]
+        target_files = file_list[filter_bounds(tile_bounds, rows_cols[:, 0], rows_cols[:, 1])]
+        if len(target_files)> 0:
+            # Get geographic bounds info
+            geo_starts = vrt_info.transform * cur_ind
+            geo_ends = list(vrt_info.transform * (cur_ind + PROCESS_BOX_SIZE))
+            geo_ends[1] = max(geo_ends[1], vrt_info.bounds.bottom)
+            geo_ends[0] = min(geo_ends[0], vrt_info.bounds.right)
+            geo_bounds = (geo_starts[0], geo_ends[1], geo_ends[0], geo_starts[1])
 
-        # avg is shape (1, height, width)
-        out_profile['height'] = avg.shape[1]
-        out_profile['width'] = avg.shape[2]
-        out_profile['transform'] = out_transform
-        with rio.open(
-            os.path.join(OUT_DIR, 'overlapped_{}_{}.tif'.format(cur_ind[0], cur_ind[1])),
-            'w',
-            **out_profile) as out_fh:
-            out_fh.write(avg.astype(np.uint8))
+            avg, out_transform = merge_avg(target_files, geo_bounds)
+            if geo_starts[0] != out_transform.c or geo_starts[1] != out_transform.f:
+                print('geo bounds:', geo_bounds)
+                print('out_transform:', out_transform)
+                raise ValueError('Output transform does not match geo bounds')
 
-# Build output vrt and translate
-sp.call('gdalbuildvrt',
-        '-co', 'BLOCKXSIZE=256',
-        '-co', 'BLOCKXSIZE=256',
-        os.path.join(OUT_DIR, 'full_overlapped.vrt'),
-        os.path.join(OUT_DIR, 'overlapped*.tif')
-        )
-sp.call('gdal_translate',
-        '-co', 'COMPRESS=LZW',
-        '-co', 'TILED=YES',
-        '-co', 'BLOCKXSIZE=256',
-        '-co', 'BLOCKYSIZE=256',
-        os.path.join(OUT_DIR, 'full_overlapped.vrt'),
-        os.path.join(OUT_DIR, 'full_overlapped.tif')
-        )
+            # avg is shape (1, height, width)
+            out_profile['height'] = avg.shape[1]
+            out_profile['width'] = avg.shape[2]
+            out_profile['transform'] = out_transform
+            with rio.open(
+                os.path.join(OUT_DIR, 'overlapped_{}_{}.tif'.format(cur_ind[0], cur_ind[1])),
+                'w',
+                **out_profile) as out_fh:
+                out_fh.write(avg.astype(np.uint8))
 
-# NOTE: Windowed writing to a big file worked for a while but hung after about 8 windows
-# So taking the more annoying but more straightforward route of writing tiles
-# with create_output('./merge_test.tif', vrt_info) as out_fh:
-#     for cur_ind in start_ind[12:]:
-#         tile_bounds = [cur_ind[0] - 1000, cur_ind[1] - 1000, cur_ind[0] + box_size + 1000, cur_ind[1] + box_size + 1000]
-#         target_files = file_list[filter_bounds(tile_bounds, rows_cols[:, 0], rows_cols[:, 1])]
-#         if len(target_files)> 0:
-#             print(target_files[0])
-#             geo_starts = vrt_info.transform * cur_ind
-#             geo_ends = list(vrt_info.transform * (cur_ind + box_size))
-#             geo_ends[1] = max(geo_ends[1], vrt_info.bounds.bottom)
-#             geo_ends[0] = min(geo_ends[0], vrt_info.bounds.right)
+    if CREATE_FULL_TIF:
+        # Build output vrt and translate
+        sp.call(
+            ['gdalbuildvrt', os.path.join(OUT_DIR, 'full_overlapped.vrt')]
+            + glob.glob(os.path.join(OUT_DIR, 'overlapped_*.tif'))
+            )
+        sp.call([
+            'gdal_translate',
+            '-co', 'COMPRESS=LZW',
+            '-co', 'TILED=YES',
+            '-co', 'BLOCKXSIZE=256',
+            '-co', 'BLOCKYSIZE=256',
+            os.path.join(OUT_DIR, 'full_overlapped.vrt'),
+            os.path.join(OUT_DIR, 'full_overlapped.tif')
+            ])
 
-#             geo_bounds = (geo_starts[0], geo_ends[1], geo_ends[0], geo_starts[1])
-#             print(geo_bounds)
-#             avg, out_trans = merge_avg(target_files, geo_bounds)
-#             print(avg.shape)
-#             print(np.max(avg.astype(np.uint8)))
-#             write_out(avg.astype(np.uint8)[0], out_fh, cur_ind[1], cur_ind[0])
 
+if __name__ == '__main__':
+    main()

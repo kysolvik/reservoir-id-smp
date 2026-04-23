4ca7ac0f70a848e6f556a61f3d9fa16e59028aac
Kylen Solvik <kysolvik@gmail.com>
2026-04-23 14:43:30 -0400

    Overlap run and summarize_landsat with Landsat 9

--- reservoir-id-smp/analysis/clean_summarize/bash_scripts/quickrun_aea.sh
+++ reservoir-id-smp/analysis/clean_summarize/bash_scripts/quickrun_aea.sh
@@ -1,25 +1,24 @@
-for f in ../../predict/full_outputs/v3/ls8*aea.tif;
+for f in ../../predict/full_outputs/v3/ls8*.tif;
 do 
     basename_f=$(basename $f)
     csv_out=out/${basename_f/tif/csv}
     time python3 find_overlaps_badwater.py $f 178 data/hydropolys_raster_byte_aea.tif $csv_out
 done
-for f in ../../predict/full_outputs/v3/ls7*aea.tif;
+for f in ../../predict/full_outputs/v3/ls7*.tif;
 do 
     basename_f=$(basename $f)
     csv_out=out/${basename_f/tif/csv}
     time python3 find_overlaps_badwater.py $f 1 data/hydropolys_raster_byte_aea.tif $csv_out
 done
-for f in ../../predict/full_outputs/v3/ls5*aea.tif;
+for f in ../../predict/full_outputs/v3/ls5*.tif;
 do 
     basename_f=$(basename $f)
     csv_out=out/${basename_f/tif/csv}
     time python3 find_overlaps_badwater.py $f 9 data/hydropolys_raster_byte_aea.tif $csv_out
 done
-
-# for f in ../../predict/full_outputs/v2/full/ls7*;
-# do 
-#     basename_f=$(basename $f)
-#     csv_out=out/${basename_f/tif/csv}
-#     time python3 find_overlaps_badwater.py $f data/hydropolys_raster_byte.tif $csv_out
-# done
+for f in ../../predict/full_outputs/v3/ls9*.tif;
+do 
+    basename_f=$(basename $f)
+    csv_out=out/${basename_f/tif/csv}
+    time python3 find_overlaps_badwater.py $f 178 data/hydropolys_raster_byte_aea.tif $csv_out
+done
